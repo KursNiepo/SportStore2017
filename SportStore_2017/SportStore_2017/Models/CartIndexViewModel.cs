851161d8a7089ceb0ac3825116466ad7fe36488f
pszczesz <pszczesz@poczta.onet.pl>
2017-06-19 12:58:33 +0200

    koszyk

---
+++ SportStore_2017/SportStore_2017/Models/CartIndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿using SportStore_2017.Models.Entities;
+
+namespace SportStore_2017.Models {
+    public class CartIndexViewModel {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
